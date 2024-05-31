0741ccafd1a3782ac435d78535e6823b4319c6e2
Anwesh Saha <anweshsaha21@iitk.ac.in>
2024-05-30 18:53:37 -0700

    NetCDF.py

--- src/NetCDF.py
+++ src/NetCDF.py
@@ -1,22 +1,12 @@
 import netCDF4
 from netCDF4 import Dataset
 
+# Load the NetCDF file
 jan = Dataset('/Users/arpitasen/Desktop/Sanghavi/Data/EMIT_L2A_RFL_001_20220903T163129_2224611_012.nc')
 reflectance = jan.variables['reflectance']
-shape = reflectance.shape
-print("Shape of reflectance variable:", shape)
-num_bands = shape[2]
-print("Number of bands:", num_bands)
+start_band = 50
+end_band = 54  # Since slicing in Python is end-exclusive, we use 54 to include band 53
+reflectance_selected_bands = reflectance[:, :, start_band:end_band]
+print(f"Shape of reflectance data for bands {start_band} to {end_band - 1}: {reflectance_selected_bands.shape}")
+print(reflectance_selected_bands)
 
-print(f"Reflectance data type: {reflectance.dtype}")
-print(f"Reflectance dimensions: {reflectance.dimensions}")
-
-band_index = 0
-reflectance_band = reflectance[:, :, band_index]
-print(f"Reflectance data for band {band_index}:")
-print(reflectance_band)
-
-for band in range(num_bands):
-    reflectance_band = reflectance[:, :, band]
-    print(f"Reflectance data for band {band}:")
-    print(reflectance_band)
